ac48922d34f77b01a411a83480721108f626eaaa
Danilo <danilo.lima@sumup.com>
2019-11-09 21:34:03 -0300

    Added guests and facilities to be processed by the next screen

--- components/SearchForm.js
+++ components/SearchForm.js
@@ -1,71 +1,61 @@
-import React, { useState } from 'react';
-import { StyleSheet, Dimensions, ScrollView, Button, Text } from 'react-native';
-import { theme, Input, Checkbox } from 'galio-framework';
-const { width } = Dimensions.get('screen');
+import React, { useState } from "react";
+import { StyleSheet, Dimensions, ScrollView, Button, Text } from "react-native";
+import { theme, Input, Checkbox } from "galio-framework";
+const { width } = Dimensions.get("screen");
+
+const initial_facilities = [
+  "whiteboard",
+  "video",
+  "postit",
+  "coffee",
+  "fridge",
+  "tv",
+  "projector"
+];
 
 const SearchForm = ({ onSubmit, ...props }) => {
-  const [ guests, setGuests ] = useState(0);
-  const [ facilities, setFacilities ] = useState([]);
-
-  const values = {
-    guests: guests,
-    facilities: facilities
-  };
-
-  const facilities = ["whiteboard", "video", "postit", "coffee", "fridge", "tv", "projector"];
+  const { navigation } = props;
+  const [guests, setGuests] = useState(null);
+  const [facilities, setFacilities] = useState([]);
 
   const handleSubmit = (guests, facilities) => {
-    setGuests(guests);
+    navigation.navigate("Elements", { guests, facilities });
+  };
 
-    if (onSubmit) {
-      onSubmit(values);
+  const toggleFacility = (item, checked) => {
+    if (checked) {
+      return setFacilities([...facilities, item]);
     }
+
+    return setFacilities(facilities.filter(facility => facility !== item));
   };
 
   return (
-      <ScrollView>
-        <Input
-          type="number-pad"
-          label="Enter the number of guests for the room:"
-          placeholder="Enter the number of guests for the room"
-          value={guests}
+    <ScrollView>
+      <Input
+        type="number-pad"
+        label="Enter the number of guests for the room:"
+        placeholder="Enter the number of guests for the room"
+        onChangeText={value => setGuests(value)}
+      />
+
+      <Text>Choose the facilities you need:</Text>
+
+      {initial_facilities.map(item => (
+        <Checkbox
+          key={item}
+          value={item}
+          label={item.toUpperCase()}
+          onChange={checked => toggleFacility(item, checked)}
         />
+      ))}
 
-        <Text>Choose the facilities you need:</Text>
-
-        {facilities.map(item => (
-          <Checkbox
-            key={item}
-            value={item}
-            label={item}
-            flexDirection="row"
-            checkboxStyle={styles.tagContainer}
-            labelStyle={styles.facilitiesTag}
-            onChange={item => console.log(item) }
-          />))
-        }
-
-        <Button onPress={handleSubmit} title="Get a room" />
-      </ScrollView>
+      <Button
+        onPress={() => handleSubmit(guests, facilities)}
+        title="Get a room"
+      />
+    </ScrollView>
   );
 };
-
-const styles = StyleSheet.create({
-  searchForm: {
-    width: width,
-    padding: '15px'
-  },
-  tagContainer: {
-    display: 'flex'
-  },
-  facilitiesTag: {
-    margin: theme.SIZES.BASE/2,
-    width: 'auto',
-    padding: theme.SIZES.BASE/2,
-    borderRadius: theme.SIZES.BASE/2,
-    backgroundColor: '#ff00ee',
-    color: '#fff'
-  }
-});
 
 export default SearchForm;
