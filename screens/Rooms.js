5756598d1d74dc744dd2a6fe2213eb3d6b90813d
Danilo <danilo.lima@sumup.com>
2019-11-10 02:33:38 -0300

    Booking feature (#5)
    * booking feature initiated
    
    * formatted code
    
    * Moved slots to state

--- screens/Rooms.js
+++ screens/Rooms.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import Intl from 'intl';
-import locale from 'intl/locale-data/jsonp/pt';
-import { StyleSheet, Dimensions, ScrollView } from 'react-native';
-import { Block, theme, Button } from 'galio-framework';
+import React from "react";
+import Intl from "intl";
+import locale from "intl/locale-data/jsonp/pt";
+import { StyleSheet, Dimensions, ScrollView } from "react-native";
+import { Block, theme, Button } from "galio-framework";
 
+import { Card } from "../components";
+const { width } = Dimensions.get("screen");
 
-import { Card } from '../components';
-const { width } = Dimensions.get('screen');
+import { findByCalendar } from "../services/rooms";
 
-import { findByCalendar } from '../services/rooms';
-import slots from '../constants/slots';
+import getFreeSlots from "../services/slots";
 
 const formatOptions = {
-  hour: '2-digit',
-  minute: '2-digit',
-  second: '2-digit',
-  hourCycle: 'h24',
-  timeZoneName: 'short'
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+  hourCycle: "h24",
+  timeZoneName: "short"
 };
 
 const formatDateTime = dateTime =>
@@ -27,42 +27,57 @@ const formatDateTime = dateTime =>
       return accumulator;
     }, []);
 
-const formatedSlots = slots.map(slot => {
-  const start = formatDateTime(slot.start);
-  const end = formatDateTime(slot.end);
+const formatSlots = slots =>
+  slots.map(slot => {
+    const start = formatDateTime(slot.start);
+    const end = formatDateTime(slot.end);
 
-  slot.start = `${start.hour}:${start.minute}`;
-  slot.end = `${end.hour}:${end.minute}`;
+    slot.startDateTime = slot.start;
+    slot.endDateTime = slot.end;
 
-  return slot;
-});
+    slot.start = `${start.hour}:${start.minute}`;
+    slot.end = `${end.hour}:${end.minute}`;
+
+    return slot;
+  });
 
 class Rooms extends React.Component {
-  
+  state = {
+    slots: []
+  };
+
+  componentDidMount() {
+    getFreeSlots(["coffee", "tv"], 5).then(slots =>
+      this.setState({ slots: formatSlots(slots) })
+    );
+  }
   renderSlots = () => {
     const { navigation } = this.props;
+
     return (
       <Block>
         <ScrollView
           showsVerticalScrollIndicator={false}
-          contentContainerStyle={styles.articles}>
+          contentContainerStyle={styles.articles}
+        >
           <Block flex>
-            
-            {formatedSlots.map(slot => (
-              <Card 
-                key={slot.room}
-                item={slot}
-                horizontal
-              />
+            {this.state.slots.map(slot => (
+              <Card key={slot.room} item={slot} horizontal />
             ))}
           </Block>
         </ScrollView>
         <Block>
-          <Button onPress={() => navigation.navigate('Pro', {facilities: ['coffee']})}>Discover</Button>
+          <Button
+            onPress={() =>
+              navigation.navigate("Pro", { facilities: ["coffee"] })
+            }
+          >
+            Discover
+          </Button>
         </Block>
       </Block>
-    )
-  }
+    );
+  };
 
   render() {
     return (
@@ -75,12 +90,12 @@ class Rooms extends React.Component {
 
 const styles = StyleSheet.create({
   home: {
-    width: width,
+    width: width
   },
   articles: {
     width: width - theme.SIZES.BASE * 2,
-    paddingVertical: theme.SIZES.BASE,
-  },
+    paddingVertical: theme.SIZES.BASE
+  }
 });
 
 export default Rooms;
--- services/rooms.js
+++ services/rooms.js
@@ -1,28 +1,63 @@
-import { intersection, filter, sortBy, compose } from 'lodash/fp';
-import { rooms } from '../server/rooms.json';
+import { intersection, filter, sortBy, compose } from "lodash/fp";
+import { rooms } from "../server/rooms.json";
+import axios from "axios";
+import { getAccessToken } from "./oauth.js";
 
+const filterByFacilities = facilities => {
+  return rooms =>
+    filter(
+      room =>
+        intersection(room.facilities, facilities).length == facilities.length,
+      rooms
+    );
+};
 
-const filterByFacilities = (facilities) => {
-    return rooms => (filter(
-        room => (
-            intersection(room.facilities, facilities).length == facilities.length
-        ), rooms)
-    )
-}
+const filterByGuests = number => {
+  return rooms => filter(room => room.guests >= number, rooms);
+};
 
-const filterByGuests = (number) => {
-    return rooms => (filter(room => (room.guests >= number), rooms))
-}
+const sortByGuest = () => rooms => sortBy(room => room.guests, rooms);
 
-const sortByGuest = () => (rooms => (sortBy(room => (room.guests), rooms)))
+export const getARoom = (facilities, guests) =>
+  compose(
+    sortByGuest(),
+    filterByGuests(guests),
+    filterByFacilities(facilities)
+  )(rooms);
 
-export const getARoom = (facilities, guests) => (
-    compose(
-        sortByGuest(),
-        filterByGuests(guests),
-        filterByFacilities(facilities)
-    )(rooms)
-);
+export const findByCalendar = calendar =>
+  filter(room => room.calendar == calendar, rooms)[0];
 
-export const findByCalendar = calendar => filter(room => room.calendar == calendar, rooms)[0]
+export const book = (item, room) => {
+  const calendarId = "danilo.lima@sumup.com";
+  const accessToken = getAccessToken();
+  const url = `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events`;
+  const data = {
+    end: {
+      dateTime: item.endDateTime,
+      timeZone: "America/Sao_Paulo"
+    },
+    start: {
+      dateTime: item.startDateTime,
+      timeZone: "America/Sao_Paulo"
+    },
+    attendees: [{ email: room.calendar }],
+    summary: "Organizer's meeting"
+  };
 
+  const options = {
+    method: "POST",
+    url: url,
+    data: data,
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/json"
+    }
+  };
+
+  console.log(JSON.stringify(data));
+
+  return axios(options)
+    .then(response => console.log(response))
+    .catch(error => console.log(error));
+};
