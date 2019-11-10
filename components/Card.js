2be84e8ec694a787b64510d853b81c427699ff05
Danilo <danilo.lima@sumup.com>
2019-11-09 23:58:25 -0300

    booking feature initiated

--- components/Card.js
+++ components/Card.js
@@ -1,38 +1,51 @@
-import React from 'react';
-import { withNavigation } from 'react-navigation';
-import PropTypes from 'prop-types';
-import { StyleSheet, Dimensions, Image, TouchableWithoutFeedback } from 'react-native';
-import { Block, Text, theme } from 'galio-framework';
+import React from "react";
+import { withNavigation } from "react-navigation";
+import PropTypes from "prop-types";
+import {
+  StyleSheet,
+  Dimensions,
+  Image,
+  TouchableWithoutFeedback
+} from "react-native";
+import { Block, Text, theme } from "galio-framework";
 
-import { argonTheme } from '../constants';
+import { argonTheme } from "../constants";
 
-import { findByCalendar } from '../services/rooms';
-
+import { findByCalendar, book } from "../services/rooms";
 
 class Card extends React.Component {
   render() {
-    const { navigation, item, horizontal, full, style, ctaColor, imageStyle } = this.props;
-    
+    const {
+      navigation,
+      item,
+      horizontal,
+      full,
+      style,
+      ctaColor,
+      imageStyle
+    } = this.props;
+
     const imageStyles = [
       full ? styles.fullImage : styles.horizontalImage,
       imageStyle
     ];
     const cardContainer = [styles.card, styles.shadow, style];
-    const imgContainer = [styles.imageContainer,
+    const imgContainer = [
+      styles.imageContainer,
       horizontal ? styles.horizontalStyles : styles.verticalStyles,
       styles.shadow
     ];
 
-    const room = findByCalendar(item.room)
+    const room = findByCalendar(item.room);
 
     return (
       <Block row={horizontal} card flex style={cardContainer}>
-        <TouchableWithoutFeedback onPress={() => console.log(room)}>
+        <TouchableWithoutFeedback onPress={() => book(item, room)}>
           <Block flex style={imgContainer}>
             <Image source={{ uri: item.image }} style={imageStyles} />
           </Block>
         </TouchableWithoutFeedback>
-        <TouchableWithoutFeedback onPress={() => console.log(room)}>
+        <TouchableWithoutFeedback onPress={() => book(item, room)}>
           <Block flex space="between" style={styles.cardDescription}>
             <Text size={14} style={styles.cardTitle}>
               {item.start} - {item.end}
@@ -63,8 +76,8 @@ Card.propTypes = {
   horizontal: PropTypes.bool,
   full: PropTypes.bool,
   ctaColor: PropTypes.string,
-  imageStyle: PropTypes.any,
-}
+  imageStyle: PropTypes.any
+};
 
 const styles = StyleSheet.create({
   card: {
@@ -76,7 +89,7 @@ const styles = StyleSheet.create({
   },
   cardTitle: {
     flex: 1,
-    flexWrap: 'wrap',
+    flexWrap: "wrap",
     paddingBottom: 6
   },
   cardDescription: {
@@ -85,18 +98,18 @@ const styles = StyleSheet.create({
   imageContainer: {
     borderRadius: 3,
     elevation: 1,
-    overflow: 'hidden',
+    overflow: "hidden"
   },
   image: {
     // borderRadius: 3,
   },
   horizontalImage: {
     height: 122,
-    width: 'auto',
+    width: "auto"
   },
   horizontalStyles: {
     borderTopRightRadius: 0,
-    borderBottomRightRadius: 0,
+    borderBottomRightRadius: 0
   },
   verticalStyles: {
     borderBottomRightRadius: 0,
@@ -110,8 +123,8 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 4,
     shadowOpacity: 0.1,
-    elevation: 2,
-  },
+    elevation: 2
+  }
 });
 
 export default withNavigation(Card);
--- services/rooms.js
+++ services/rooms.js
@@ -1,28 +1,63 @@
-import { intersection, filter, sortBy, compose } from 'lodash/fp';
-import { rooms } from '../server/rooms.json';
+import { intersection, filter, sortBy, compose } from "lodash/fp";
+import { rooms } from "../server/rooms.json";
+import axios from "axios";
 
+const filterByFacilities = facilities => {
+  return rooms =>
+    filter(
+      room =>
+        intersection(room.facilities, facilities).length == facilities.length,
+      rooms
+    );
+};
 
-const filterByFacilities = (facilities) => {
-    return rooms => (filter(
-        room => (
-            intersection(room.facilities, facilities).length == facilities.length
-        ), rooms)
-    )
-}
+const filterByGuests = number => {
+  return rooms => filter(room => room.guests >= number, rooms);
+};
 
-const filterByGuests = (number) => {
-    return rooms => (filter(room => (room.guests >= number), rooms))
-}
+const sortByGuest = () => rooms => sortBy(room => room.guests, rooms);
 
-const sortByGuest = () => (rooms => (sortBy(room => (room.guests), rooms)))
+export const getARoom = (facilities, guests) =>
+  compose(
+    sortByGuest(),
+    filterByGuests(guests),
+    filterByFacilities(facilities)
+  )(rooms);
 
-export const getARoom = (facilities, guests) => (
-    compose(
-        sortByGuest(),
-        filterByGuests(guests),
-        filterByFacilities(facilities)
-    )(rooms)
-);
+export const findByCalendar = calendar =>
+  filter(room => room.calendar == calendar, rooms)[0];
 
-export const findByCalendar = calendar => filter(room => room.calendar == calendar, rooms)[0]
+export const book = (item, room) => {
+  const calendarId = "danilo.lima@sumup.com";
+  const accessToken =
+    "ya29.ImWwB2TbRezdMcadLPviOHeegVKyHBDwwP22VLtQscFtOSm8XIC7J4BFq9sFFOZL7aD7x4thwSr0NnMgCEvKrk-mOdR_qodRg3j1HOzTLn32iPhEwMKSZpCgwfEMguVxcqXjBO2IIw";
+  const url = `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events`;
+  const data = {
+    end: {
+      dateTime: item.end,
+      timeZone: "America/Sao_Paulo"
+    },
+    start: {
+      dateTime: item.start,
+      timeZone: "America/Sao_Paulo"
+    },
+    attendees: [{ email: room.calendar }],
+    summary: "Organizer's meeting"
+  };
 
+  const options = {
+    method: "POST",
+    url: url,
+    data: data,
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/json"
+    }
+  };
+
+  console.log(options);
+
+  return axios(options)
+    .then(response => console.log(response))
+    .catch(error => console.log(error));
+};
